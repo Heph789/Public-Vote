d649ca793939ea12263862aea5cc6cb96db4e031
Chase Jeter <chaselbjeter@gmail.com>
2018-05-30 13:39:46 -0400

    Finished testing for the 'establishVote' function of 'PublicVote.sol'

--- truffle/test/PublicVote.test.js
+++ truffle/test/PublicVote.test.js
@@ -1,10 +1,14 @@
+
+var throws = require('./helpers/expectThrow.js');
+
 var PublicVote = artifacts.require("PublicVote");
 
 contract('PublicVote', function(accounts) {
   describe('Estabilishing the vote', () => {
     var owner = accounts[0];
+    const name = [...Buffer.from("Hillary v Donald", "ascii")];
+
     it('should establish a vote setting owner to the parameter', () => {
-      const name = [...Buffer.from("Hillary v Donald", "ascii")];
       const numOfItems = 10;
       var vote;
 
@@ -20,5 +24,31 @@ contract('PublicVote', function(accounts) {
       });
 
     });
+
+    it('should not allow establishing a vote of the same name', () => {
+      var vote;
+      return PublicVote.deployed().then((pv) => {
+        vote = pv;
+        return throws.expectThrow(vote.establishVote(name, 10, {from: accounts[1]}));
+      })
+      .then((state)=>{
+        assert(state, "Expected error not thrown");
+      });
+
+    });
+
+    it('should not allow establishing a vote with more than 19 candidates', () => {
+      var vote;
+      return PublicVote.deployed().then((pv) => {
+        vote = pv;
+        return throws.expectThrow(vote.establishVote([0], 20, {from: accounts[1]}));
+      })
+      .then((state) => {
+        assert(state, "Expected error not thrown");
+      });
+
+    });
+
   });
+
 });
